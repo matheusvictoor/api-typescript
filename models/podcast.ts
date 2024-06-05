7048ea5988563b192d2d9930c3f6a52740c80627
Matheus Victor <matheusvictor.dev@gmail.com>
2024-06-05 12:09:15 -0300

    feat: adiciona as models

---
+++ models/podcast.ts
@@ -0,0 +1,11 @@
+import { Core } from "./core";
+
+/**
+* Model de podcast
+* @type url {String} url do podcast
+* @type duração {String} tempo do audio
+*/
+export class Podcast extends Core {
+  url: String;
+  duracao: String;
+}
